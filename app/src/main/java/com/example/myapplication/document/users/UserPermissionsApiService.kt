e009b41db8fe8e0c37931f6aaa1c269718e8e6c8
anleonov <anton.leonov18@gmail.com>
2018-08-09 17:24:13 +0300

    Add permission endpoints

--- app/src/main/java/com/example/myapplication/document/users/UserPermissionsApiService.kt
+++ app/src/main/java/com/example/myapplication/document/users/UserPermissionsApiService.kt
@@ -1,13 +1,26 @@
 package com.example.myapplication.document.users
 
-import com.example.myapplication.document.DocumentCommentDto
+import com.example.myapplication.document.User
+import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface UserPermissionsApiService {
 
     @GET("api/v1/permissions")
     fun getUsersPerDocument(@Query("documentId") documentId: String,
                               @Query("scope") scope: String = "document"): Call<Permission>
+
+    @GET("api/v1/users")
+    fun getUsersByParameters(@Query("groupId") groupId: String? = null,
+                             @Query("query") query: String?,
+                             @Query("includeDisabled") includeDisabled: Boolean): Call<List<User>>
+
+    @POST("api/v1/permissions")
+    fun createOrUpdateUserAndGroupPermissionForDocument(@Path("documentId") documentId: String,
+                                                        @Body dto: Permission): Call<Permission>
+
+    @DELETE
+    fun deletePermission(@Path("permissionId") permissionId: String): Call<ResponseBody>
+
 }
