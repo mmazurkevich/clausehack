2d50384ad4eb163145329026cd85d6f39968a9e2
anleonov <anton.leonov18@gmail.com>
2018-08-09 13:33:42 +0300

    Separate approvals for paragraphs and documents

--- app/src/main/java/com/example/myapplication/approval/ApprovalFragment.kt
+++ app/src/main/java/com/example/myapplication/approval/ApprovalFragment.kt
@@ -10,6 +10,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.myapplication.ClauseMatchApplication
 import com.example.myapplication.R
 import com.example.myapplication.gson
@@ -23,6 +24,8 @@ class ApprovalFragment: Fragment() {
     private lateinit var viewAdapter: ApprovalListItemAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var swipeContainer: SwipeRefreshLayout
+
+    private var scope: Scope = Scope.paragraph
 
     private val LOADED_CONTENT = "APPROVAL_LIST"
 
@@ -39,17 +42,24 @@ class ApprovalFragment: Fragment() {
             adapter = viewAdapter
         }
 
-        val loadedApprovals = PreferenceManager.getDefaultSharedPreferences(context)
-                .getString(LOADED_CONTENT, "")
-        if (loadedApprovals != null && loadedApprovals.isNotBlank()) {
-            viewAdapter.mValues = gson.fromJson(loadedApprovals, Array<ApprovalPendingDto>::class.java).toList()
-            viewAdapter.notifyDataSetChanged()
-        } else {
+        loadApprovals()
+
+        val paragraphApprovalButton = view.findViewById<Button>(R.id.paragraph_approval_button)
+        paragraphApprovalButton.setOnClickListener {
+            scope = Scope.paragraph
+            loadApprovals()
+        }
+
+        val documentApprovalButton = view.findViewById<Button>(R.id.document_approval_button)
+        documentApprovalButton.setOnClickListener {
+            scope = Scope.document
             loadApprovals()
         }
 
         swipeContainer = view.findViewById(R.id.approval_refresh)
-        swipeContainer.setOnRefreshListener { loadApprovals() }
+        swipeContainer.setOnRefreshListener {
+            loadApprovalsFromRemote()
+        }
 
         swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
                 android.R.color.holo_green_light,
@@ -60,18 +70,31 @@ class ApprovalFragment: Fragment() {
     }
 
     private fun loadApprovals() {
+        val loadedApprovals = PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(LOADED_CONTENT, "")
+        if (loadedApprovals != null && loadedApprovals.isNotBlank()) {
+            val approvals = gson.fromJson(loadedApprovals, Array<ApprovalPendingDto>::class.java).toList()
+            viewAdapter.mValues = filterApprovalsByScope(scope, approvals)
+            viewAdapter.notifyDataSetChanged()
+        } else {
+            loadApprovalsFromRemote()
+        }
+    }
+
+    private fun loadApprovalsFromRemote() {
         val approvalService = (this.activity!!.application as ClauseMatchApplication).approvalService
-        val approvals = approvalService.getApprovals()
-        approvals.enqueue(approvalsLoadCallback)
+        val approvalCallback = approvalService.getApprovals()
+        approvalCallback.enqueue(approvalsLoadCallback)
     }
 
     private val approvalsLoadCallback: Callback<List<ApprovalPendingDto>> = object : Callback<List<ApprovalPendingDto>> {
 
         override fun onResponse(call: Call<List<ApprovalPendingDto>>, response: Response<List<ApprovalPendingDto>>) {
-            val approvals = response.body()
+            val approvals = response.body() ?: emptyList()
 
-            viewAdapter.mValues = approvals
+            viewAdapter.mValues = filterApprovalsByScope(scope, approvals)
             viewAdapter.notifyDataSetChanged()
+
             swipeContainer.isRefreshing = false
 
             PreferenceManager
@@ -83,6 +106,10 @@ class ApprovalFragment: Fragment() {
         override fun onFailure(call: Call<List<ApprovalPendingDto>>, t: Throwable) {
             Log.d("APPROVAL_ACTIVITY", t.stackTrace.toString())
         }
+    }
+
+    private fun filterApprovalsByScope(scope: Scope, approvals: List<ApprovalPendingDto>): List<ApprovalPendingDto> {
+        return approvals.filter { it.approval.scope == scope }
     }
 
 }
